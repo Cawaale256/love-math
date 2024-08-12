d4b39865443498459f72a9dd1c294f140fcbb110
Cawaale256 <abdi.awale5@gmail.com>
2024-08-12 22:02:17 +0000

    Display question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,42 +10,62 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
+
+    runGame("addition");
+
 });
 
 /**
- * The main game "loop",called when the script is first loaded
+ * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame(){
-    // Creates two random number between 1 and 25
-    let num1 = math.floor(math.random()*25)+1;
-    let num2 = math.floor(math.random()*25)+1;
- }
+function runGame(gameType) {
 
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 
- function checkAnswer(){
+}
 
- }
- calculateCorrectAnswer(){
+function checkAnswer() {
 
- }
- function incrementScore(){
+}
 
- }
- function incrementWrongAnswer(){ 
+function calculateCorrectAnswer() {
 
- }
- function displayAdditionQuestion(){
+}
 
- }
- function displaySubtractQuestion(){
+function incrementScore() {
 
- }
- function displayMultiplQuestion(){
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
     
- }
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
